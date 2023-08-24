54e1c63d699116efb0d600e59041e8767ebb7953
Vitor Lopes Nunes <vitorlopes040805@gmail.com>
2023-08-24 09:00:53 -0300

    Carrinho

---
+++ src/pages/Carrinho/index.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { Text, View, Image } from "react-native";
+import topo from '../../../assets/Logo3.png'
+
+export default function Carrinho() {
+  return <View>
+      <Image source={topo}/>
+  </View>;
+}
