4d3ab82320a8a87857c8c673a03a98ca58f5fb9e
Vitor Lopes Nunes <vitorlopes040805@gmail.com>
2023-09-06 15:51:41 -0300

    inicio

--- App.tsx
+++ App.tsx
@@ -1,13 +1,28 @@
+import {
+  useFonts, 
+  Quicksand_400Regular,
+  Quicksand_600SemiBold
+} from "@expo-google-fonts/quicksand";
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
 import Carrinho from './src/pages/Carrinho';
 
 export default function App() {
+  const [fonteCarregada] = useFonts({
+    QRegular: Quicksand_400Regular,
+    QSemiBold: Quicksand_600SemiBold
+  });
+
+  if (!fonteCarregada) {
+    return null;
+  }
+
   return (
-    <View>
+    <SafeAreaView style={{flex: 1}}>
+    <StatusBar />
       <Carrinho/>
-    </View>
+    </SafeAreaView>
   );
 }
 
--- src/pages/Carrinho/index.tsx
+++ src/pages/Carrinho/index.tsx
@@ -1,9 +1,32 @@
 import React from "react";
-import { Text, View, Image } from "react-native";
-import topo from '../../../assets/Logo3.png'
+import { StyleSheet, Text, View, Image, Dimensions } from "react-native";
+import topo from "../../../assets/Logo3.png";
+
+const width = Dimensions.get("screen").width;
 
 export default function Carrinho() {
-  return <View>
-      <Image source={topo}/>
-  </View>;
+  return (
+    <>
+      <View>
+        <Image source={topo} style={styles.topo} />
+      </View>
+      <View>
+        <Text style={styles.titulo}>Mama Mia</Text>
+      </View>
+    </>
+  );
 }
+
+const styles = StyleSheet.create({
+  topo: {
+    width: "100%",
+    height: (578 / 768) * width,
+  },
+  titulo: {
+    color: "black",
+    fontSize: 24,
+    lineHeight: 26,
+    padding: 16,
+    fontFamily: "QSemiBold",
+  }
+});
