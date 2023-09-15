77c3413226bbe48ef96a5c6f4629561bfdb37540
Vitor Lopes Nunes <vitorlopes040805@gmail.com>
2023-09-15 11:11:06 -0300

    styles

--- src/pages/Carrinho/index.tsx
+++ src/pages/Carrinho/index.tsx
@@ -1,7 +1,10 @@
 import React from "react";
 import { StyleSheet, Text, View, Image, Dimensions } from "react-native";
-import topo from "../../../assets/Logo3.png";
-import marguerita from "../../../assets/pizzaMarguerita.png"
+import topo from "../../../assets/Logo4.png";
+import marguerita from "../../../assets/Pizza-Margherita-1024x685.jpg";
+import quatroQueijos from "../../../assets/4queijos.jpg";
+import frango from "../../../assets/Pizza-de-Frango-com-Catupiry.jpg";
+import { LinearGradient } from "expo-linear-gradient";
 
 const width = Dimensions.get("screen").width;
 
@@ -9,37 +12,78 @@ export default function Carrinho() {
   return (
     <>
       <View style={styles.bg}>
-        <View>
+        <LinearGradient
+          style={{
+            width: "100%",
+            height: (578 / 768) * width,
+          }}
+          start={{ x: 0, y: 1 }}
+          end={{ x: 1, y: 0 }}
+          colors={["#900000", "#151515"]}
+        >
           <Image source={topo} style={styles.topo} />
-        </View>
+        </LinearGradient>
 
-        <View>
-          <Text style={styles.titulo}>Mama Mia</Text>
+        <View style={styles.body}>
           <View>
-            <Text style={styles.descricao}>
-              As pizzas mais italianas da Etec ou algo assim
-            </Text>
+            <Text style={styles.titulo}>Mama Mia</Text>
+            <View>
+              <Text style={styles.descricao}>
+                As pizzas mais italianas da Etec ou algo assim
+              </Text>
+            </View>
           </View>
-        </View>
 
-        <View style={styles.cardapio}>
+          <View style={styles.cardapio}>
+            <View style={styles.pizzaCard}>
+              <View style={styles.pizzaTextos}>
+                <Text style={styles.PizzaNome}>Marguerita</Text>
+                <View>
+                  <Text style={styles.PizzaDescricao}>
+                    Mussarela, Tomate e Manjericão
+                  </Text>
+                  <Text style={styles.PizzaPreco}>R$ 59.90</Text>
+                </View>
+              </View>
 
-          <View style={styles.pizzaCard}>
-            <View style={styles.pizzaTextos}>
-              <Text style={styles.PizzaNome}>Marguerita</Text>
-              <View>
-              <Text style={styles.PizzaDescricao}>Formaggio, Pomodoro, Bsilico e mais nada texto para teste</Text>
-              <Text style={styles.PizzaPreco}>R$ 59.90</Text>
+              <View style={styles.fotoPizzaDiv}>
+                <Image source={marguerita} style={styles.fotoPizza} />
               </View>
             </View>
 
-            <View style={styles.fotoPizzaDiv}>
-            <Image source={marguerita} style={styles.fotoPizza} />
+            <View style={styles.pizzaCard}>
+              <View style={styles.pizzaTextos}>
+                <Text style={styles.PizzaNome}>Quatro Quejos</Text>
+                <View>
+                  <Text style={styles.PizzaDescricao}>
+                    Mussarela, Provolone, Parmesão, Gorgonzola
+                  </Text>
+                  <Text style={styles.PizzaPreco}>R$ 59.90</Text>
+                </View>
+              </View>
+
+              <View style={styles.fotoPizzaDiv}>
+                <Image source={quatroQueijos} style={styles.fotoPizza} />
+              </View>
+            </View>
+
+            <View style={styles.pizzaCard}>
+              <View style={styles.pizzaTextos}>
+                <Text style={styles.PizzaNome}>Frango c/ Catupiry</Text>
+                <View>
+                  <Text style={styles.PizzaDescricao}>
+                    Mussarela, Frango e Catupiry
+                  </Text>
+                  <Text style={styles.PizzaPreco}>R$ 59.90</Text>
+                </View>
+              </View>
+
+              <View style={styles.fotoPizzaDiv}>
+                <Image source={frango} style={styles.fotoPizza} />
+              </View>
             </View>
           </View>
-
         </View>
-
       </View>
     </>
   );
@@ -47,22 +91,24 @@ export default function Carrinho() {
 
 const styles = StyleSheet.create({
   bg: {
-    /**  backgroundColor: "red", */
     height: "100%",
   },
   topo: {
     width: "100%",
     height: (578 / 768) * width,
   },
+  body: {
+    backgroundColor: "#303030",
+  },
   titulo: {
-    color: "black",
+    color: "#f1f1f1",
     fontSize: 24,
     lineHeight: 26,
     padding: 16,
     fontFamily: "QSemiBold",
   },
   descricao: {
-    color: "black",
+    color: "#f1f1f1",
     fontSize: 16,
     lineHeight: 16,
     paddingLeft: 16,
@@ -70,56 +116,54 @@ const styles = StyleSheet.create({
     fontFamily: "QRegular",
   },
   cardapio: {
-    color: "black",
     fontSize: 24,
     lineHeight: 26,
     padding: 16,
     gap: 10,
   },
   pizzaCard: {
-    backgroundColor: "gray",
+    backgroundColor: "#606060",
     borderRadius: 10,
     display: "flex",
     flexDirection: "row",
     justifyContent: "space-between",
-    padding: 8
+    padding: 8,
+    maxHeight: 150,
   },
   pizzaTextos: {
     display: "flex",
     flexDirection: "column",
-    marginRight: "25%"
+    marginRight: "25%",
   },
   PizzaNome: {
-    color: "black",
+    color: "#f1f1f1",
     fontSize: 24,
     lineHeight: 26,
+    marginRight: "25%",
     fontFamily: "QSemiBold",
   },
   PizzaDescricao: {
-    color: "black",
+    color: "#f1f1f1",
     fontSize: 16,
     lineHeight: 16,
     marginRight: "25%",
     fontFamily: "QRegular",
   },
   PizzaPreco: {
-    color: "green",
-    fontSize: 16,
-    lineHeight: 16,
-    paddingLeft: 16,
-    paddingRight: 16,
-    fontFamily: "QRegular",
+    color: "#009930",
+    fontSize: 20,
+    lineHeight: 26,
+    fontFamily: "QSemiBold",
+    marginTop: "16.7%",
   },
-
-  /** Terminar de ajeitar o tamanho da imagem */
-
   fotoPizza: {
-    height: 152,
-    width: 152,
-    backgroundColor: "red",
+    height: 150,
+    width: 150,
+    borderTopRightRadius: 10,
+    borderBottomRightRadius: 10,
   },
   fotoPizzaDiv: {
     position: "absolute",
-    marginLeft: "62.5%"
-  }
+    marginLeft: "63%",
+  },
 });
